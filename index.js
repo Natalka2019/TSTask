b721be669fa17e84a94a424003d9c8771f42fdbf
Nataliya Rushchak <nrushchak@gmail.com>
2022-06-24 22:13:12 +0300

    fetch all posts and selected post

---
+++ index.js
@@ -0,0 +1,87 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+;
+const allPostsURL = 'https://jsonplaceholder.typicode.com/posts';
+const filteredPostsURL = 'https://jsonplaceholder.typicode.com/posts/';
+const postsList = document.querySelector('.postsList');
+const filterButton = document.querySelector('.filterButton');
+const showAllButton = document.querySelector('.showAllButton');
+const input = document.querySelector('.actions__input');
+const errorMessage = document.querySelector('.error');
+input.addEventListener("keypress", (e) => {
+    if (e.key === "Enter") {
+        createSelectedPost(+input.value);
+    }
+});
+filterButton.addEventListener("click", () => {
+    createSelectedPost(+input.value);
+});
+showAllButton.addEventListener("click", () => {
+    input.value = '';
+    createAllPostsList();
+});
+function createAllPostsList() {
+    return __awaiter(this, void 0, void 0, function* () {
+        postsList.innerHTML = "";
+        const posts = yield fetchData(allPostsURL);
+        if (posts) {
+            if (posts.length > 0) {
+                const allPosts = posts.map(post => renderPost(post)).join('');
+                postsList.innerHTML = allPosts;
+            }
+            else {
+                postsList.innerHTML = "No posts available";
+            }
+        }
+    });
+}
+;
+function createSelectedPost(id) {
+    return __awaiter(this, void 0, void 0, function* () {
+        postsList.innerHTML = "";
+        const post = yield fetchData(`${filteredPostsURL}${id}`);
+        postsList.innerHTML = renderPost(post);
+    });
+}
+;
+function fetchData(URL) {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            const response = yield fetch(URL);
+            const data = yield response.json();
+            if (!Array.isArray(data) && Object.keys(data).length === 0) {
+                throw Error();
+            }
+            else {
+                return data;
+            }
+        }
+        catch (e) {
+            console.log(e);
+            errorMessage.innerHTML = 'Oooops! Something went wrong. Please, try again later.';
+        }
+    });
+}
+;
+function renderPost(post) {
+    if (Object.keys(post).length !== 0) {
+        const { userId, id, title, body } = post;
+        return `<li class="post" id=${id}>
+            <p class="post__userId">User:${userId}</p>
+            <h4 class="post__title">${title}</h4>
+            <p class="post__id">Post id:${id}</p>
+            <p class="post__body">${body}</p>
+          </li>`;
+    }
+    else {
+        return `<li class="post">There is no such post</li>`;
+    }
+}
---
+++ index.ts
@@ -0,0 +1,92 @@
+interface IPost {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+};
+
+const allPostsURL = 'https://jsonplaceholder.typicode.com/posts';
+const filteredPostsURL = 'https://jsonplaceholder.typicode.com/posts/';
+
+const postsList = document.querySelector('.postsList') as HTMLElement;
+const filterButton = document.querySelector('.filterButton') as HTMLButtonElement;
+const showAllButton = document.querySelector('.showAllButton') as HTMLButtonElement;
+const input = document.querySelector('.actions__input') as HTMLInputElement;
+const errorMessage = document.querySelector('.error') as HTMLElement;
+
+input.addEventListener("keypress", (e: KeyboardEvent) => {
+  if (e.key === "Enter") {
+    createSelectedPost(+input.value);
+  }
+});
+
+filterButton.addEventListener("click", () => {
+  createSelectedPost(+input.value);
+});
+
+showAllButton.addEventListener("click", () => {
+  input.value = '';
+  createAllPostsList();
+});
+
+async function createAllPostsList (): Promise<void>  {
+  postsList.innerHTML = "";
+
+  const posts = await fetchData(allPostsURL) as IPost[];
+
+  if(posts) {
+    if(posts.length > 0) {
+      const allPosts = posts.map(post => renderPost(post)).join('');
+  
+      postsList.innerHTML = allPosts;
+    } else {
+      postsList.innerHTML = "No posts available";
+    }
+  }
+
+};
+
+async function createSelectedPost (id: number): Promise<void> {
+  postsList.innerHTML = "";
+
+  const post = await fetchData(`${filteredPostsURL}${id}`) as IPost;
+  
+  postsList.innerHTML = renderPost(post);
+};
+
+async function fetchData<T>(URL: string): Promise<T | void> {
+  try {
+    const response = await fetch(URL);
+    const data = await response.json();
+
+    if(!Array.isArray(data) && Object.keys(data).length === 0) {
+      throw Error();
+    } else {
+      return data;
+    }
+
+  } catch (e) {
+    console.log(e);
+
+    errorMessage.innerHTML = 'Oooops! Something went wrong. Please, try again later.';
+
+  }
+};
+
+
+function renderPost (post: IPost): string {
+
+if(Object.keys(post).length !== 0) {
+  const {userId, id, title, body} = post;
+  
+  return `<li class="post" id=${id}>
+            <p class="post__userId">User:${userId}</p>
+            <h4 class="post__title">${title}</h4>
+            <p class="post__id">Post id:${id}</p>
+            <p class="post__body">${body}</p>
+          </li>`
+} else {
+  return `<li class="post">There is no such post</li>`
+}
+
+}
